d12d5fedfd59ff8d6efede7473eea6c05b0ff526
Leo Godin <leo.g.godin@intel.com>
2013-03-27 14:40:38 -0700

    First iteration of product listing

---
+++ depot/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Depot::Application.config.secret_key_base = '3973709cd2176acbb68032beaead6b572bd4527777862bdeec416987c72779d6cd15026d53fdb6d16074c9291c53af73b0d163913525e0e3884b36afdb066b89'
---
+++ work/demo/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Demo::Application.config.secret_key_base = 'cb8ea3fbb4a2a6a3b79151c37221868645184079275e0e22c31e4cd01521ed52c8791c2ac029dd6f82d848e27845a53aae0e0abce037465a4dc5298dc07e6b94'
